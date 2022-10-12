946858118df627003cc0a12842d0b8b905f9c8ea
Jason <el.jsteer@gmail.com>
2022-10-12 21:28:35 +1100

    added basic file structure for project, still need to clear out old code for apollo and server stuff etc

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import Matchup from './pages/Matchup';
+import Vote from './pages/Vote';
+import NotFound from './pages/NotFound';
+
+
+function App() {
+  return (
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />}
+            />
+            <Route 
+              path="/matchup" 
+              element={<Matchup />}
+            />
+            <Route 
+              path="/matchup/:id" 
+              element={<Vote />}
+            />
+            <Route 
+              path="*"
+              element={<NotFound />}
+            />
+          </Routes>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
