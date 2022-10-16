64f535d20d3e82df9de8c85346d7147c34cf7189
Mackenzie Gray <mdkulen@gmail.com>
2022-10-16 13:17:51 +1100

    added client/utils/localStorage

---
+++ client/src/utils/localStorage.js
@@ -0,0 +1,33 @@
+// function to retrieve saved projects 
+export const getSavedProjectIds = () => {
+    const savedProjectIds = localStorage.getItem('saved_projects')
+    ? JSON.parse(localStorage.getItem('saved_projects'))
+    : [];
+
+    return savedProjectIds;
+};
+
+// function to save projects by Id
+export const saveProjectIds = (projectIdArr) => {
+    if (projectIdArr.length) {
+    localStorage.setItem('saved_projects', JSON.stringify(projectIdArr));
+    } else {
+        localStorage.removeItem('saved_projects');
+    }
+};
+
+// function to remove project 
+export const removeProjectId = (projectId) => {
+    const savedProjectIds = localStorage.getItem('saved_projects')
+    ? JSON.parse(localStorage.getItem('saved_projects'))
+    : null;
+
+    if (!savedProjectIds) {
+    return false;
+    }
+
+    const updatedSavedProjectIds = savedProjectIds?.filter((savedProjectId) => savedProjectId !== projectId);
+    localStorage.setItem('saved_books', JSON.stringify(updatedSavedProjectIds));
+
+    return true;
+};
