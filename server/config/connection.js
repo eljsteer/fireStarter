52852f05508f265fb84b37d727404e5d6b5fbe36
Jason <el.jsteer@gmail.com>
2023-01-21 15:10:32 +1100

    updated config for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // connect with mongoose 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/firestarter", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/firestarter", {
   useNewUrlParser: true,
   useUnifiedTopology: false,
 });
